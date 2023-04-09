ecd540ecb8b213690444c2289ed130a63db6b4dc
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-10 02:43:28 +0530

    last commit

--- src/components/botsec.jsx
+++ src/components/botsec.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Botsec = () => {
   return (
-    <div className='w-[40vw] bg-pink-300'>Botsec</div>
+    <div className='w-[40vw] bg-pink-300'>
+      <div className='bg-rose-400'> s</div>
+      <div className='bg-rose-800 mt-10'> b</div>
+
+    </div>
   )
 }
 
