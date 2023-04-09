aee4035ff23df4a7d5e78b98bc137e0fc3ffaa1f
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-10 02:40:56 +0530

    topsec done

--- src/components/topsec.jsx
+++ src/components/topsec.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Topsec = () => {
   return (
     <>
-          <div className='w-[40vw] bg-rose-400'> 
-            <div></div>
+          <div className='w-[40vw] h-[33vh] grid place-items-center'> 
+          <div className='grid place-items-center mt-20'>
+            <div className='text-5xl pb-10'>Speech To Text Converter</div>
+            <div className='text-gray-400 text-center'>A React Hook that converts speech from the microphone to text and makes it available to your React Components</div>
+          </div>
           </div>
     </>
   )
